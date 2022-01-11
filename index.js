a07c305385778143ec92df1377cf285cf7617875
Buck Randall <tbr446@gmail.com>
2022-01-11 16:11:15 -0600

    netlify test 2

--- index.js
+++ index.js
@@ -203,21 +203,21 @@ router.hooks({
         ? capitalize(params.page)
         : "Home";
 
-    if (page === "Home") {
-      axios
-        .get(
-          `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st.%20louis`
-        )
-        .then(response => {
-          state.Home.weather = {};
-          state.Home.weather.city = response.data.name;
-          state.Home.weather.temp = response.data.main.temp;
-          state.Home.weather.feelsLike = response.data.main.feels_like;
-          state.Home.weather.description = response.data.weather[0].main;
-          done();
-        })
-        .catch(err => console.log(err));
-    }
+    // if (page === "Home") {
+    //   axios
+    //     .get(
+    //       `https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=st.%20louis`
+    //     )
+    //     .then(response => {
+    //       state.Home.weather = {};
+    //       state.Home.weather.city = response.data.name;
+    //       state.Home.weather.temp = response.data.main.temp;
+    //       state.Home.weather.feelsLike = response.data.main.feels_like;
+    //       state.Home.weather.description = response.data.weather[0].main;
+    //       done();
+    //     })
+    //     .catch(err => console.log(err));
+    // }
 
     if (page === "Home") {
       axios
